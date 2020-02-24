df4ed7fb5e3ffc752f8e294320193ccd00417ba9
unknown <david.montaine@snet.net>
2020-02-23 20:22:20 -0500

    docker, pom changes

--- git-assume.cmd
+++ git-assume.cmd
@@ -4,6 +4,7 @@ IF "%1"=="true" (
     SET action=assume-unchanged
 )
 git update-index --%action% build-run.cmd
+git update-index --%action% cacerts.jks
 git update-index --%action% Dockerfile
 git update-index --%action% domain.xml
 git update-index --%action% git-assume.cmd
